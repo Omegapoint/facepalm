1d0412658666bae6b6ee1e6843850bf2beea6d0c
Daniel Hultqvist <daniel.hultqvist@omegapoint.se>
2016-01-11 22:14:36 +0100

    Removed old unused DocumentRepository + implementation

--- src/main/java/se/omegapoint/facepalm/domain/repository/DocumentRepository.java
+++
@@ -1,9 +0,0 @@
-package se.omegapoint.facepalm.domain.repository;
-
-import se.omegapoint.facepalm.infrastructure.db.Document;
-
-import java.util.List;
-
-public interface DocumentRepository {
-    List<Document> findByUsername(String username);
-}
